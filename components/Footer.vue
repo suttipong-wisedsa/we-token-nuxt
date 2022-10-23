9a79ffe8d70c3ebfe8f05dfbdf061deb00ab78bd
suttipong-wisedsa <64350049+suttipong-wisedsa@users.noreply.github.com>
2022-10-23 14:02:13 +0700

    footer-update

--- components/Footer.vue
+++ components/Footer.vue
@@ -14,29 +14,33 @@
         </v-col>
         <v-spacer class="hidden-xs-only"></v-spacer>
         <v-spacer class="hidden-xs-only"></v-spacer>
-        <v-col class="mx-10">
+        <v-col class="mx-10 d-md-non text-center">
+           <v-col class="mx-10">
           <h2 class="white--text my-5">Company</h2>
           <p class="white--text my-5">About</p>
           <p class="white--text my-5">Our Partner</p>
         </v-col>
+        </v-col>
         <v-spacer class="d-md-none .d-lg-flex"></v-spacer>
-        <v-col>
-          <h2 class="white--text my-5">Support</h2>
-          <p class="white--text my-5">We Token NEWS</p>
-          <p class="white--text my-5">FAQ</p>
+        <v-col class="mx-10 d-md-non text-center">
+           <v-col class="mx-10">
+          <h2 class="white--text my-5">Company</h2>
+          <p class="white--text my-5">About</p>
+          <p class="white--text my-5">Our Partner</p>
+        </v-col>
         </v-col>
         <v-spacer class="d-md-none .d-lg-flex"></v-spacer>
         <v-col class="hidden-xs-only">
           <h2 class="white--text my-4">Social Media</h2>
-          <v-col class="d-flex align-center" style="height: 4rem">
+          <v-col class="d-flex align-center" style="height: 3rem">
             <img src="../static/facebook.png" />
             <p class="white--text my-3 mx-2">Company</p>
           </v-col>
-          <v-col class="d-flex align-center" style="height: 4rem">
+          <v-col class="d-flex align-center" style="height: 3rem">
             <img src="../static/ig.png" />
             <p class="white--text my-4 mx-2">About</p>
           </v-col>
-          <v-col class="d-flex align-center" style="height: 4rem">
+          <v-col class="d-flex align-center" style="height: 3rem">
             <img src="../static/youtube.png" />
             <p class="white--text my-4 mx-2">Our Partner</p>
           </v-col>
